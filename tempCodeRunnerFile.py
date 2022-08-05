b43428e3041ef56283b070b054ca87831db3ed72
Nick_Z <73448659+nickz7@users.noreply.github.com>
2022-08-05 15:42:19 +0800

    Add files via upload

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+for i in dir_list:
+    #     if malware_checker(i) != 0:
+    #         print(i)
+    #         virusName.append(malware_checker(i)+" :: File :: "+i)
+    #         virusPath.append(i)
---
+++ test.py
@@ -0,0 +1,4 @@
+
+for i in range(0,10):
+    io = [1,2,3,4]
+    print(io)
